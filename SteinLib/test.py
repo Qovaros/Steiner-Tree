12dba5c7f67f98e3cb5840700f71f87e59c33d48
Qovaros <macieknems@gmail.com>
2020-08-03 02:08:58 +0200

    change result format to .csv and generate more results

--- SteinLib/test.py
+++ SteinLib/test.py
@@ -1,9 +1,15 @@
 import os
 import sys
+import subprocess
 
 program_name = sys.argv[1]
 test_folder = 'test/'
 
 for directory in os.listdir(test_folder):
+    os.system('echo name,nodes,edges,terminals,distances time, copy time, dreyfus-wagner time,time,result >> results/' + directory + '.csv ')
     for f in sorted(os.listdir(test_folder + directory)):
-        os.system(program_name + ' ' + test_folder + directory + '/' + f + ' results/' + directory + '.out ' + f)
+        print(f)
+        os.system("echo -n " + f.split(".")[0] + ', >> results/' + directory + '.csv ')
+        result = os.system("timeout 31s " + program_name + ' < ' + test_folder + directory + '/' + f + ' >> results/' + directory + '.csv')
+        if result != 0:
+            os.system('echo n/a,n/a,n/a,over 30s,n/a >> results/' + directory + '.csv ')
